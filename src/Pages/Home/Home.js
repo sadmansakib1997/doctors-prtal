026ad10cb2ef5c4dfe909d90702bd3c1910fd6ed
sadmansakib1997 <sadmansakib670@gmail.com>
2022-10-28 01:10:19 +0600

    another-item

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -11,20 +11,18 @@ const Home = () => {
           <img
             style={{ height: "500px" }}
             className="d-block w-100 rounded"
-            src="https://img.freepik.com/free-photo/programmer-scanning-screen-his-smartwatch-with-smartphone-camera_1098-18710.jpg?w=740&t=st=1666802920~exp=1666803520~hmac=013dbf060117597d70990b0969919109471251128b38e445f9c1b87f7c0d1e57"
+            src="https://thumbs.dreamstime.com/b/phone-hand-python-language-symbol-white-mobile-screen-coding-software-concept-abstract-digital-wireframe-low-poly-155898716.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
-            <p className="fw-bold">
-              “asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf“asgdasgdgasdfgasgfasgf
-            </p>
+            <p className="fw-bold"></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             style={{ height: "500px" }}
             className="d-block w-100 rounded"
-            src="https://img.freepik.com/free-vector/digital-coding-background-with-numbers-zero-one_1017-30363.jpg?t=st=1666802906~exp=1666803506~hmac=9856a6224b93b7fa7f6ef25c37d7a4c6c7d82a8e33f67076dac1696af19ab5ef"
+            src="https://content.instructables.com/ORIG/FTP/GXQA/HM768QN0/FTPGXQAHM768QN0.jpg?auto=webp"
             alt="Second slide"
           />
 
@@ -36,7 +34,20 @@ const Home = () => {
           <img
             style={{ height: "500px" }}
             className="d-block w-100 rounded"
-            src="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=740&t=st=1666803004~exp=1666803604~hmac=9f6ed11ca0cad2159f692ded5e48cab2a99c07ea0f4637b6517187c15f08a792"
+            src="https://i.pinimg.com/originals/3c/af/d1/3cafd1bfdbc7191aaf49981b3e3d0594.png"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>PHP</h3>
+            <p className="fw-bold"></p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ width: "626px", height: "500px" }}
+            className="d-block w-100 rounded"
+            src="https://www.careergirls.org/wp-content/uploads/2015/06/Computer_Programmer1920X10180.jpg"
             alt="Third slide"
           />
 
