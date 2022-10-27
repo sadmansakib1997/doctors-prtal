61f7add5e9959864b33ec696170d8da32b15fd60
sadmansakib1997 <sadmansakib670@gmail.com>
2022-10-27 22:54:49 +0600

    home

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -4,7 +4,7 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <section className="d-flex align-items-center h-100 p-16  text-secondary">
-      This Is our Crying Error Page
+      This Is our Error Page
     </section>
   );
 };
