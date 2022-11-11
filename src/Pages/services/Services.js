d89b6df460bbae931a367fbb11731841c906452e
sadmansakib1997 <sadmansakib670@gmail.com>
2022-11-11 19:04:43 +0600

    thirteen

--- src/Pages/services/Services.js
+++ src/Pages/services/Services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Container, Row, Spinner } from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 import Service from "../service/Service";
 
 const Services = () => {
